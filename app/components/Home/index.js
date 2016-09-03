d42576142ba41663639a179e88298bfaf0655008
Le Van Nghia <nghialv.bk@gmail.com>
2016-09-03 15:28:56 +0700

    styling a React component

--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -1,11 +1,20 @@
 import React from "react"
+import cssModules from "react-css-modules"
+import style from "./style.css"
+import Sidebar from "../Sidebar"
+
 
 export class Home extends React.Component {
 	render(){
 		return (
-			<div>Home Component</div>
+			<div>
+				<Sidebar />
+				<div className={style.chatWrapper}>
+					Home Component
+				</div>
+			</div>
 		)
 	}
 }
 
-export default Home
+export default cssModules(Home, style)
