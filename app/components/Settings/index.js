32643caf552f79464daa7d04c64fc2bb179f1bc2
Le Van Nghia <nghialv.bk@gmail.com>
2016-09-03 08:38:05 +0700

    React Router

---
+++ app/components/Settings/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export class Settings extends React.Component {
+	render(){
+		return (
+			<div>Settings Component</div>
+		)
+	}
+}
+
+export default Settings
---
+++ app/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { Router, Route, IndexRoute, hashHistory } from "react-router"
+
+import Home from "./components/Home"
+import Settings from "./components/Settings"
+
+const App = props => (<div>{props.children}</div>)
+
+// class HelloWorld extends React.Component {
+// 	render() {
+// 		return (<div>HelloWorld</div>)
+// 	}
+// }
+
+ReactDOM.render(
+	<Router history={hashHistory}>
+	  <Route path="/" component={App}>
+	  	<IndexRoute component={Home} />
+	  	<Route path="settings" component={Settings} />
+	  </Route>
+	</Router>,
+	document.getElementById("root")
+)
